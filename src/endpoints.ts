ddf378083e7c25233f04a64b2d5f3c3620df4ee6
ethan-keller <e.keller@student.tudelft.nl>
2022-01-23 21:29:30 +0100

    Add server communication

--- src/endpoints.ts
+++ src/endpoints.ts
@@ -1,4 +1,4 @@
-const baseUrl = "----";
+// const baseUrl = "http://192.168.1.126:5000";
+const baseUrl = "http://172.20.10.4:5000"
 
-export const getMedication = baseUrl + "/";
-export const isLasa = baseUrl + "/"
+export const checkLasa = baseUrl + "/check_lasa"
--- src/server_communication/api.ts
+++ src/server_communication/api.ts
@@ -8,7 +8,7 @@ class API {
 	}
 
 	private static async request<T>(endpoint: string, params: Object): Promise<T> {
-		const response: Response = await fetch(endpoint + this.paramsToQueryString(params));
+    let response: Response = await fetch(endpoint + "?" + this.paramsToQueryString(params));
 
     if (response.status >= 200 && response.status < 300) {
       return response.json();
@@ -16,12 +16,9 @@ class API {
       return Promise.reject(new Error(response.statusText));
     }
 	}
-  public static async getMedication(code: string): Promise<MedicationData> {
-    return this.request(endpoints.getMedication, {code: code})
-  }
 
-  public static async isLasa(code: string): Promise<boolean> {
-    return this.request(endpoints.isLasa, {code: code})
+  public static async checkLasa(code: string): Promise<MedicationData> {
+    return this.request(endpoints.checkLasa, {medicationCode: code})
   }
 }
 
