e67e12ae24418c4bc8afbbab58857320256dbe76
ethan-keller <e.keller@student.tudelft.nl>
2022-01-24 13:14:23 +0100

    Add settings

--- src/endpoints.ts
+++ src/endpoints.ts
@@ -1,4 +1,4 @@
 // const baseUrl = "http://192.168.1.126:5000";
-const baseUrl = "http://172.20.10.4:5000"
+const baseUrl = "http://145.94.229.236:5000"
 
 export const checkLasa = baseUrl + "/check_lasa"
